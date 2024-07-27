8109f25d391e6ff7432cd2b3d70d9fc1ede36410
Biniam Aregay <seifubiniam6@gmail.com>
2024-07-28 01:10:11 +0300

    Building a Dynamic Content Generator with Advanced DOM Manipulation-1

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
   function showRandomQuote() {
     const randomIndex = Math.floor(Math.random() * quotes.length);
     const randomQuote = quotes[randomIndex];
-    quoteDisplay.textContent = `"${randomQuote.text}" - ${randomQuote.category}`;
+    quoteDisplay.innerHTML = `"${randomQuote.text}" - ${randomQuote.category}`;
   }
 
   // Function to add a new quote
